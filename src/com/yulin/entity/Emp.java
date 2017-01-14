22965b5053f5604bb3eb4a9bb5d8e0c5778c3070
toast <lntu_tony@yeah.net>
2017-01-14 17:53:45 +0800

    3:修改一次Emp

--- src/com/yulin/entity/Emp.java
+++ src/com/yulin/entity/Emp.java
@@ -2,12 +2,20 @@ package com.yulin.entity;
 
 public class Emp {
 	private String name;
-
+	private String hello;
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getHello() {
+		return hello;
+	}
+
+	public void setHello(String hello) {
+		this.hello = hello;
 	}
 }
