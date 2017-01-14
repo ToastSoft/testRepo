d30d198b79b50f7ed595e6a3375c0290f82b46e1
toast <lntu_tony@yeah.net>
2017-01-14 20:43:34 +0800

    #在admin中添加内容，这不会有冲突

--- src/com/yulin/entity/Admin.java
+++ src/com/yulin/entity/Admin.java
@@ -1,5 +1,14 @@
 package com.yulin.entity;
 
 public class Admin {
+	//admin中添加了一些内容
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
