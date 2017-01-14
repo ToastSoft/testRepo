19d3375478b22730292f6dbd7bb8e0a6b4fd3fb1
toast <lntu_tony@yeah.net>
2017-01-14 20:49:23 +0800

    #删除admin属性

--- src/com/yulin/entity/Admin.java
+++ src/com/yulin/entity/Admin.java
@@ -2,15 +2,7 @@ package com.yulin.entity;
 
 public class Admin {
 	//admin中添加了一些内容
-	private String name;
 	private String age;
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public String getAge() {
 		return age;
