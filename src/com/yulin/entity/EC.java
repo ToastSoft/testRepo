8a25f457190eb819d3420832d0aa090d7df24112
toast <lntu_tony@yeah.net>
2017-01-14 20:17:31 +0800

    修改EC

--- src/com/yulin/entity/EC.java
+++ src/com/yulin/entity/EC.java
@@ -1,5 +1,14 @@
 package com.yulin.entity;
 
 public class EC {
+	//TIAN
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
